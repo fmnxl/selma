1d7c7f04c522f4b5c7d00c65586431bfd565f670
Freeman Latif <freemanon@gmail.com>
2019-09-01 18:19:40 +0100

    prettify

--- Main.hs
+++ Main.hs
@@ -52,45 +52,39 @@ import Brick.Widgets.Core
   , withBorderStyle
   )
 import System.Random
-width = 30
 
-g2 :: IO [Int]
-g2 = do
-  g <- newStdGen
-  return $ randomRs (0, width - 2) g
-
-data CustomEvent = Counter deriving Show
+data CustomEvent = Tick deriving Show
 
 data State =
-    State { _stLastBrickEvent :: Maybe (BrickEvent () CustomEvent)
-       , _stCounter :: Int
-       , _snake :: [(Int, Int)]
-       , _stVelocity :: (Int, Int)
-       , _stNextVelocity :: (Int, Int)
-       , _stFoods :: [(Int, Int)]
-       , _stGameOver :: Bool
-       }
+  State { _stLastBrickEvent :: Maybe (BrickEvent () CustomEvent)
+        , _snake :: [(Int, Int)]
+        , _stVelocity :: (Int, Int)
+        , _stNextVelocity :: (Int, Int)
+        , _stFoods :: [(Int, Int)]
+        , _stGameOver :: Bool
+        }
 
 makeLenses ''State
 
-cell color = markup ("  " @@ bg color)
+-- Screen width
+width = 30
+
+-- Infinite list of random ints
+randomInts :: IO [Int]
+randomInts = do
+  g <- newStdGen
+  return $ randomRs (0, width - 2) g
 
 initialSnakeLength = 5
 initialSnake = reverse [(x,0) | x <- [0..initialSnakeLength]]
 
-renderCell coords st
-    | coords `elem` st^.snake = cell V.blue
-    | coords `elem` st^.stFoods = cell V.red
-    | otherwise = cell V.black
-
--- This is like virtual dom
 drawUI :: State -> [Widget ()]
-drawUI st = [grid]
-    where
-        grid =
-          withBorderStyle unicode $
-            borderWithLabel (str "Snake") $
-              if st^.stGameOver then drawGameOver else drawGame st
+drawUI st = [ui]
+  where
+    ui =
+      withBorderStyle unicode $
+        borderWithLabel (str "Snake") $
+          if st^.stGameOver then drawGameOver else drawGame st
 
 drawGameOver =
   hLimit (width * 2)
@@ -104,19 +98,24 @@ drawGameOver =
 drawGame st =
   vBox [
     hBox [
-      renderCell (x, y) st
+      drawGameCell (x, y) st
       | x <- [0..width-1]] 
     | y <- [0..width-1]
   ]
 
+drawGameCell coords st
+  | coords `elem` st^.snake = cell V.blue
+  | coords `elem` st^.stFoods = cell V.red
+  | otherwise = cell V.black
+  where cell color = markup ("  " @@ bg color)
+
 move (dx,dy) (x,y) = ((x+dx) `mod` width, (y+dy) `mod` width)
 
-moveSnake grow newHead (oldHead:oldTail) = newHead:oldHead:(newTail)
-          where
-            newTail = if grow then oldTail else init oldTail
+moveSnake grow newHead (oldHead:oldTail) = newHead:oldHead:newTail
+  where
+    newTail = if grow then oldTail else init oldTail
 
-opposite_direction (x,y ) = (-x, -y)
-
+opposite_direction (x, y) = (-x, -y)
 
 potentiallyTurn currDirection direction
   | currDirection == opposite_direction(direction) = id
@@ -129,41 +128,40 @@ isGoingToDie nextHeadPos snake = nextHeadPos `elem` snake
 -- Monad transformers
 loop st = do
   newFoodPos <- liftIO potentiallyRegenerateFood
-  continue $ st & stCounter %~ (+1)
-                & snake .~ newSnake
+  continue $ st & snake .~ newSnake
                 & stFoods .~ newFoodPos
                 & stVelocity .~ st^.stNextVelocity
                 & stGameOver .~ willDie
-    where
-        (oldSnakeHead:_) = st^.snake
-        newSnakeHead = move (st^.stNextVelocity) oldSnakeHead
-        eaten = isFoodEaten (st^.stFoods) newSnakeHead
-        newSnake = moveSnake eaten newSnakeHead (st^.snake)
-        willDie = isGoingToDie newSnakeHead (st^.snake)
-        potentiallyRegenerateFood
-          | isFoodEaten (st^.stFoods) newSnakeHead = do
-            coords <- generateRandomFreeCoords st
-            return [coords]
-          | otherwise = return (st^.stFoods)
+  where
+    (oldSnakeHead:_) = st^.snake
+    newSnakeHead = move (st^.stNextVelocity) oldSnakeHead
+    eaten = isFoodEaten (st^.stFoods) newSnakeHead
+    newSnake = moveSnake eaten newSnakeHead (st^.snake)
+    willDie = isGoingToDie newSnakeHead (st^.snake)
+    potentiallyRegenerateFood
+      | isFoodEaten (st^.stFoods) newSnakeHead = do
+        coords <- generateRandomFreeCoords st
+        return [coords]
+      | otherwise = return (st^.stFoods)
 
 appEvent :: State -> BrickEvent () CustomEvent -> EventM () (Next State)
 appEvent st e =
-    case e of
-        VtyEvent (V.EvKey V.KEsc []) -> halt st
-        VtyEvent (V.EvKey (V.KChar ' ') []) -> do
-          newInitialState <- liftIO generateInitialState
-          continue $ newInitialState
-        VtyEvent (V.EvKey V.KUp []) -> continue $ st & potentiallyTurn (st^.stVelocity) (0, -1)
-        VtyEvent (V.EvKey V.KDown []) -> continue $ st & potentiallyTurn (st^.stVelocity) (0, 1)
-        VtyEvent (V.EvKey V.KLeft []) -> continue $ st & potentiallyTurn (st^.stVelocity) (-1, 0)
-        VtyEvent (V.EvKey V.KRight []) -> continue $ st & potentiallyTurn (st^.stVelocity) (1, 0)
-        VtyEvent _ -> continue $ st & stLastBrickEvent .~ (Just e)
-        AppEvent Counter -> if (st^.stGameOver) then continue st else loop st
-        _ -> continue st
+  case e of
+    VtyEvent (V.EvKey V.KEsc []) -> halt st
+    VtyEvent (V.EvKey (V.KChar ' ') []) -> do
+      newInitialState <- liftIO generateInitialState
+      continue $ newInitialState
+    VtyEvent (V.EvKey V.KUp []) -> continue $ st & potentiallyTurn (st^.stVelocity) (0, -1)
+    VtyEvent (V.EvKey V.KDown []) -> continue $ st & potentiallyTurn (st^.stVelocity) (0, 1)
+    VtyEvent (V.EvKey V.KLeft []) -> continue $ st & potentiallyTurn (st^.stVelocity) (-1, 0)
+    VtyEvent (V.EvKey V.KRight []) -> continue $ st & potentiallyTurn (st^.stVelocity) (1, 0)
+    VtyEvent _ -> continue $ st & stLastBrickEvent .~ (Just e)
+    AppEvent Tick -> if (st^.stGameOver) then continue st else loop st
+    _ -> continue st
 
 generateRandomCoords = do
-    [x,y] <- g2 >>= \x -> return $ take 2 $ x
-    return (x,y)
+  [x,y] <- randomInts >>= \x -> return $ take 2 $ x
+  return (x,y)
 
 generateRandomFreeCoords st = do
   let allItems = (st^.snake) ++ (st^.stFoods)
@@ -183,7 +181,6 @@ generateInitialState :: IO State
 generateInitialState = do
   randomPos <- generateRandomFreeCoordsExcept initialSnake
   return $ State { _stLastBrickEvent = Nothing
-                 , _stCounter = 0
                  , _snake = initialSnake
                  , _stVelocity = (1, 0)
                  , _stNextVelocity = (1, 0)
@@ -193,23 +190,23 @@ generateInitialState = do
 
 theApp :: App State CustomEvent ()
 theApp =
-    App { appDraw = drawUI
-        , appChooseCursor = showFirstCursor -- what this?
-        , appHandleEvent = appEvent
-        , appStartEvent = return
-        , appAttrMap = const $ attrMap V.defAttr []
-        }
+  App { appDraw = drawUI
+      , appChooseCursor = showFirstCursor -- what this?
+      , appHandleEvent = appEvent
+      , appStartEvent = return
+      , appAttrMap = const $ attrMap V.defAttr []
+      }
 
 main :: IO ()
 main = do
-    chan <- newBChan 10
-    x <- return (Just 10)
+  chan <- newBChan 10
+  x <- return (Just 10)
 
-    forkIO $ forever $ do
-        threadDelay 200000
-        writeBChan chan Counter
+  forkIO $ forever $ do
+    threadDelay 200000
+    writeBChan chan Tick
 
-    let buildVty = V.mkVty V.defaultConfig
-    initialVty <- buildVty
-    iS <- generateInitialState
-    void $ customMain initialVty buildVty (Just chan) theApp iS
+  let buildVty = V.mkVty V.defaultConfig
+  initialVty <- buildVty
+  iS <- generateInitialState
+  void $ customMain initialVty buildVty (Just chan) theApp iS
